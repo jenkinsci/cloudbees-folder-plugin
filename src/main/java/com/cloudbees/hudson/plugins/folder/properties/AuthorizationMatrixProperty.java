2fb997a73f1ab21c8b5608bda18a85a6d6f1c910
Jesse Glick <jglick@cloudbees.com>
2015-05-05 15:13:48 -0400

    More general to check for PermissionScope.ITEM_GROUP. This pulls in SCM.PERMISSIONS too, which is what we want. Also it is consistent with hudson.security.AuthorizationMatrixProperty (which checks PermissionScope.ITEM).

--- src/main/java/com/cloudbees/hudson/plugins/folder/properties/AuthorizationMatrixProperty.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/properties/AuthorizationMatrixProperty.java
@@ -34,12 +34,10 @@ import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.model.Hudson;
-import hudson.model.Item;
-import hudson.model.Run;
-import hudson.model.View;
 import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.security.Permission;
 import hudson.security.PermissionGroup;
+import hudson.security.PermissionScope;
 import hudson.security.ProjectMatrixAuthorizationStrategy;
 import hudson.security.SidACL;
 import hudson.util.FormValidation;
@@ -52,6 +50,7 @@ import org.kohsuke.stapler.StaplerRequest;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -172,7 +171,13 @@ public class AuthorizationMatrixProperty extends FolderProperty<Folder> {
         }
 
         public List<PermissionGroup> getAllGroups() {
-            return Arrays.asList(PermissionGroup.get(Item.class), PermissionGroup.get(Run.class), PermissionGroup.get(View.class));
+            List<PermissionGroup> groups = new ArrayList<PermissionGroup>();
+            for (PermissionGroup g : PermissionGroup.getAll()) {
+                if (g.hasPermissionContainedBy(PermissionScope.ITEM_GROUP)) {
+                    groups.add(g);
+                }
+            }
+            return groups;
         }
 
         public boolean showPermission(Permission p) {
