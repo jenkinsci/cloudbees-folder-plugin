de3411ca9f920b881066074d70868d3ea6493d95
recena <mrecena@cloudbees.com>
2016-04-19 16:00:17 +0200

    [JENKINS-34200] New API to set a custom successful destination

--- src/main/java/com/cloudbees/hudson/plugins/folder/AbstractFolder.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/AbstractFolder.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2015 CloudBees, Inc.
+ * Copyright 2015-2016 CloudBees, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -40,9 +40,6 @@ import hudson.model.AllView;
 import hudson.model.Descriptor;
 import hudson.model.HealthReport;
 import hudson.model.Item;
-import static hudson.model.Item.CONFIGURE;
-import static hudson.model.Item.CREATE;
-import static hudson.model.Item.DELETE;
 import hudson.model.ItemGroup;
 import static hudson.model.ItemGroupMixIn.loadChildren;
 import hudson.model.Items;
@@ -705,8 +702,19 @@ public abstract class AbstractFolder<I extends TopLevelItem> extends AbstractIte
             if (namingStrategy.isForceExistingJobs()) {
                 namingStrategy.checkName(name);
             }
-            FormApply.success(".").generateResponse(req, rsp, this);
+            FormApply.success(getSuccessfulDestination()).generateResponse(req, rsp, this);
         }
+    }
+
+    /**
+     * Where user will be redirected after creating a {@code AbstractFolder}.
+     *
+     * @return A string that represents the redirect location URL.
+     *
+     * @see javax.servlet.http.HttpServletResponse#sendRedirect(String)
+     */
+    protected String getSuccessfulDestination() {
+        return ".";
     }
 
     protected void submit(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException, Descriptor.FormException {}
