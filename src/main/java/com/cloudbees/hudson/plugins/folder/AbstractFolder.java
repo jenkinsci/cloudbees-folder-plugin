7b132953106cab243334992ff527b4bd65b08e71
Jesse Glick <jglick@cloudbees.com>
2016-05-18 21:40:00 -0400

    [FIXED JENKINS-34939] Hold the lock on this folder only while deleting itself, not its children.

--- src/main/java/com/cloudbees/hudson/plugins/folder/AbstractFolder.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/AbstractFolder.java
@@ -609,9 +609,12 @@ public abstract class AbstractFolder<I extends TopLevelItem> extends AbstractIte
     }
 
     @Override
-    protected void performDelete() throws IOException, InterruptedException {
+    public void delete() throws IOException, InterruptedException {
+        checkPermission(DELETE);
         // delete individual items first
         // (disregard whether they would be deletable in isolation)
+        // JENKINS-34939: do not hold the monitor on this folder while deleting them
+        // (thus we cannot do this inside performDelete)
         SecurityContext orig = ACL.impersonate(ACL.SYSTEM);
         try {
             for (Item i : new ArrayList<Item>(items.values())) {
@@ -627,7 +630,11 @@ public abstract class AbstractFolder<I extends TopLevelItem> extends AbstractIte
         } finally {
             SecurityContextHolder.setContext(orig);
         }
-        super.performDelete();
+        synchronized (this) {
+            performDelete();
+        }
+        getParent().onDeleted(AbstractFolder.this);
+        Jenkins.getInstance().rebuildDependencyGraphAsync();
     }
 
     @Override
