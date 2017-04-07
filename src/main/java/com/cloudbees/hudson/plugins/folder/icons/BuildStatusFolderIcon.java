628f7c8c165d103735ae4367bf602913bee1b949
Fabio Gallonetto <fabio@futureworkshops.com>
2017-04-07 13:53:07 +0100

    Incorporate pull request feedback

--- src/main/java/com/cloudbees/hudson/plugins/folder/icons/BuildStatusFolderIcon.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/icons/BuildStatusFolderIcon.java
@@ -8,77 +8,77 @@ import hudson.model.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.Stapler;
 
-/**
- * Created by dune on 09/09/15.
- */
+public class BuildStatusFolderIcon extends FolderIcon {
 
-    public class BuildStatusFolderIcon extends FolderIcon {
+    Folder folder;
 
-        Folder folder;
+    @DataBoundConstructor
+    public BuildStatusFolderIcon() {
+    }
 
-        @DataBoundConstructor
-        public BuildStatusFolderIcon() {
-        }
+    public Integer ordinal() {
+      return this.getCombinedBallColor().ordinal();
+    }
 
-        public Integer ordinal() {
-          return this.getCombinedBallColor().ordinal();
-        }
-
-        private BallColor getCombinedBallColor() {
-            Boolean taskIsBuilding = false;
-            Boolean oneTaskIsIncomplete = false;
-            Result combinedResult = null;
-            for(Job job : this.folder.getAllJobs()) {
-                Run build = job.getLastBuild();
-                if(build == null) {
-                    oneTaskIsIncomplete = true;
-                    continue;
-                }
-
-                if(build.isBuilding()) {
-                    taskIsBuilding = true;
-                    build = job.getLastCompletedBuild();
-                    if(build == null) continue;
-                }
-                Result result = build.getResult();
-                if(combinedResult == null) {
-                    combinedResult = result;
-                } else {
-                    combinedResult = combinedResult.combine(result);
-                }
+    private BallColor getCombinedBallColor() {
+        Boolean taskIsBuilding = false;
+        Boolean oneTaskIsIncomplete = false;
+        Result combinedResult = null;
+        for(Job job : this.folder.getAllJobs()) {
+            Run build = job.getLastBuild();
+            if(build == null) {
+                oneTaskIsIncomplete = true;
+                continue;
             }
 
-            BallColor cumulativeIcon = null;
-            if(oneTaskIsIncomplete) {
-                cumulativeIcon = BallColor.NOTBUILT;
-            } else if(combinedResult != null) {
-                cumulativeIcon = combinedResult.color;
+            if(build.isBuilding()) {
+                taskIsBuilding = true;
+                build = job.getLastCompletedBuild();
+                if(build == null) continue;
             }
-            if(taskIsBuilding) {
-                cumulativeIcon = cumulativeIcon.anime();
+            if (oneTaskIsIncomplete) {
+                break;
             }
-            return cumulativeIcon;
+            Result result = build.getResult();
+            if(combinedResult == null) {
+                combinedResult = result;
+            } else {
+                combinedResult = combinedResult.combine(result);
+            }
         }
 
-        public String getImageOf(String size) {
-            BallColor cumulativeIcon = this.getCombinedBallColor();
-            return Stapler.getCurrentRequest().getContextPath()+ Hudson.RESOURCE_PATH+"/images/"+size+ "/" + cumulativeIcon.getImage();
-        }
+        BallColor cumulativeIcon = null;
+        if(oneTaskIsIncomplete) {
+            cumulativeIcon = BallColor.NOTBUILT;
+        } else {
+            assert combinedResult != null;
+            cumulativeIcon = combinedResult.color;
+        } 
 
+        if(taskIsBuilding) {
+            cumulativeIcon = cumulativeIcon.anime();
+        }
+        return cumulativeIcon;
+    }
+
+    public String getImageOf(String size) {
+        BallColor cumulativeIcon = this.getCombinedBallColor();
+        return Stapler.getCurrentRequest().getContextPath()+ Hudson.RESOURCE_PATH+"/images/"+size+ "/" + cumulativeIcon.getImage();
+    }
+
+    @Override
+    protected void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+
+    public String getDescription() {
+        return Messages.BuildStatusFolderIcon_Description();
+    }
+
+    public static class DescriptorImpl extends FolderIconDescriptor {
         @Override
-        protected void setFolder(Folder folder) {
-            this.folder = folder;
-        }
-
-        public String getDescription() {
-            return "Folder";
-        }
-
-        @Extension(ordinal=100)
-        public static class DescriptorImpl extends FolderIconDescriptor {
-            @Override
-            public String getDisplayName() {
-                return "Build Status Icon";
-            }
+        public String getDisplayName() {
+            return Messages.BuildStatusFolderIcon_DisplayName();
         }
     }
+}
