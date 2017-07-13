65d607f69f532d4e7e3adf4e2727aaa9f2006d41
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-07-13 12:54:49 +0100

    [JDK-8170447] This is the only {@link} to an annotation I could find

--- src/main/java/com/cloudbees/hudson/plugins/folder/TransientFolderActionFactory.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/TransientFolderActionFactory.java
@@ -42,7 +42,7 @@ import jenkins.model.TransientActionFactory;
  * Do so by storing a file under {@link Folder#getRootDir()}.
  *
  * <p>
- * To register your implementation, put {@link Extension} on your subtype.
+ * To register your implementation, put {@code @Extension} on your subtype.
  *
  * @see Action
  * @deprecated Use {@link TransientActionFactory} on {@link Folder} instead.
