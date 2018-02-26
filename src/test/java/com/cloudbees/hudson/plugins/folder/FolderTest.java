6fdcb82914839b60946643270e19829f1af35ade
Devin Nusbaum <d.w.nusbaum@gmail.com>
2018-02-26 11:25:38 -0500

    Update to latest SNAPSHOT

--- src/test/java/com/cloudbees/hudson/plugins/folder/FolderTest.java
+++ src/test/java/com/cloudbees/hudson/plugins/folder/FolderTest.java
@@ -87,10 +87,10 @@ public class FolderTest {
 
         String oldName = f.getName();
 
-        HtmlForm cfg = r.createWebClient().getPage(f, "rename2").getFormByName("config");
+        HtmlForm cfg = r.createWebClient().getPage(f, "confirm-rename").getFormByName("config");
         cfg.getInputByName("newName").setValueAttribute("newName");
         for (HtmlForm form : r.submit(cfg).getForms()) {
-            if (form.getActionAttribute().equals("doRename2")) {
+            if (form.getActionAttribute().equals("confirmRename")) {
                 r.submit(form);
                 break;
             }
