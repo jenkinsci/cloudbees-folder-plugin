990d3b9fd56c2dfda98eff8b4fbda451563337ab
Devin Nusbaum <dwnusbaum@users.noreply.github.com>
2018-06-25 04:27:29 -0400

    [JENKINS-22936] Remove old duplicated rename logic in favor of new rename action (#118)
    * Use rename logic from AbstractItem
    * Fix FolderTest#rename
    * Update to latest SNAPSHOT
    * Update to 2.110
    * Update ComputedFolder to use #checkRename and add test
    * Simplify test and recheck the name after the computation completes

--- src/test/java/com/cloudbees/hudson/plugins/folder/FolderTest.java
+++ src/test/java/com/cloudbees/hudson/plugins/folder/FolderTest.java
@@ -87,10 +87,10 @@ public class FolderTest {
 
         String oldName = f.getName();
 
-        HtmlForm cfg = r.createWebClient().getPage(f, "configure").getFormByName("config");
-        cfg.getInputByName("_.name").setValueAttribute("newName");
+        HtmlForm cfg = r.createWebClient().getPage(f, "confirm-rename").getFormByName("config");
+        cfg.getInputByName("newName").setValueAttribute("newName");
         for (HtmlForm form : r.submit(cfg).getForms()) {
-            if (form.getActionAttribute().equals("doRename")) {
+            if (form.getActionAttribute().equals("confirmRename")) {
                 r.submit(form);
                 break;
             }
