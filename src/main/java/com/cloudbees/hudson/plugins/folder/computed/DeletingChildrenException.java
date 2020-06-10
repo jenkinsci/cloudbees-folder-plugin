84158ce2a0a420876f2acb2ce3e3825bdb53b058
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2020-06-10 11:33:22 +0200

    [JENKINS-62632] Extend IOException and limit the number of messages.

--- src/main/java/com/cloudbees/hudson/plugins/folder/computed/DeletingChildrenException.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/computed/DeletingChildrenException.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DeletingChildrenException extends RuntimeException {
+public class DeletingChildrenException extends IOException {
 
     private List<IOException> exceptions;
 
@@ -13,8 +13,14 @@ public class DeletingChildrenException extends RuntimeException {
     }
 
     @Override
+    public synchronized Throwable getCause() {
+        // It's only created if it has exceptions
+        return this.exceptions.get(0);
+    }
+
+    @Override
     public String getMessage() {
-        return "Impossible to delete some children when computing the folder: \n\t" +
-                String.join("\n\t", exceptions.stream().map(e -> e.getMessage()).collect(Collectors.toList()));
+        return "Impossible to delete some children when computing the folder. The first errors are: \n\t" +
+                String.join("\n\t", exceptions.stream().limit(3).map(e -> e.getMessage()).collect(Collectors.toList()));
     }
 }
