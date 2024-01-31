eeeed28cbd23d0e8e9e25be4ffa295cc9387a7f9
Yaroslav Afenkin <yaroslavafenkin@users.noreply.github.com>
2024-01-31 11:32:58 +0200

    Address review feedback

--- src/main/java/com/cloudbees/hudson/plugins/folder/properties/FolderCredentialsProvider.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/properties/FolderCredentialsProvider.java
@@ -524,7 +524,7 @@ public class FolderCredentialsProvider extends CredentialsProvider {
             checkPermission(CredentialsProvider.UPDATE);
             Map<Domain, List<Credentials>> domainCredentialsMap = getDomainCredentialsMap();
             if (domainCredentialsMap.containsKey(domain)) {
-                if (current instanceof IdCredentials && replacement instanceof IdCredentials) {
+                if (current instanceof IdCredentials || replacement instanceof IdCredentials) {
                     if (!current.equals(replacement)) {
                         throw new IllegalArgumentException("Credentials' IDs do not match, will not update.");
                     }
