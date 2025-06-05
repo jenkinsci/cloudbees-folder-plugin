02dba1ffab3478a5e4d747bafd51573517951740
Markus Winter <m.winter@sap.com>
2025-06-05 21:20:32 +0200

    add symbol for folder credentisl store

--- src/main/java/com/cloudbees/hudson/plugins/folder/properties/FolderCredentialsProvider.java
+++ src/main/java/com/cloudbees/hudson/plugins/folder/properties/FolderCredentialsProvider.java
@@ -236,7 +236,7 @@ public class FolderCredentialsProvider extends CredentialsProvider {
      */
     @Override
     public String getIconClassName() {
-        return "icon-folder-store";
+        return "symbol-folder-store-outline plugin-cloudbees-folder";
     }
 
     /**
@@ -581,7 +581,7 @@ public class FolderCredentialsProvider extends CredentialsProvider {
             @Override
             public String getIconClassName() {
                 return isVisible()
-                        ? "icon-folder-store"
+                        ? "symbol-folder-store-outline plugin-cloudbees-folder"
                         : null;
             }
 
